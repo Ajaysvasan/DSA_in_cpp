1120c59c2a35a560f6d74d3af548f5eff87c1357
Ajaysvasan <ajay192006@gmail.com>
2025-03-23 20:55:46 +0530

    quick revision

--- revision/mergeSort_revision_one.cpp
+++ revision/mergeSort_revision_one.cpp
@@ -1,29 +1,29 @@
 #include<iostream>
 #include<vector>
+
 using namespace std;
-void merge(vector<int>& arr,int left,int mid,int right){
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    vector<int> L(n1),R(n2);
 
-    for(int i= 0;i<n1;++i){
-        L[i] = arr[i+left];
-    }
-    for(int j = 0;j<n2;++j){
-        R[j] = arr[j+mid +1 ];
-    }
-
-    int i = 0,j = 0, k = left;
-    while(i<n1 && j<n2){
+void merge(vector<int>&arr,int low,int mid,int high){
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    vector<int> L(n1) , R(n2);
+    for(int i = 0;i<n1;i++)
+        L[i] = arr[i+low];
+    for(int j = 0;j<n2;j++)
+        R[j] = arr[j+mid+1];
+    
+    int i = 0 , j = 0, k = low;
+    while(i<n1 && j < n2){
         if(L[i]<=R[j]){
             arr[k] = L[i];
             ++i;
-        }else{
+        }
+        else{
             arr[k] = R[j];
             ++j;
         }
         ++k;
-    } 
+    }
     while(i<n1){
         arr[k] = L[i];
         ++i;
@@ -36,24 +36,17 @@ void merge(vector<int>& arr,int left,int mid,int right){
     }
 }
 
-void mergeSort(vector<int>&arr,int left,int right){
-    if(left>=right) return;
-    int mid = left + (right - left) /2;
-    mergeSort(arr,left,mid);
-    mergeSort(arr,mid+1,right);
-    merge(arr,left,mid,right);
-}
-
-void print(vector<int>arr){
-    for(int num:arr){
-        cout<<num<<" ";
-    }
-    cout<<endl;
+void mergeSort(vector<int>&arr,int low,int high){
+    if(low>=high) return ;
+    int mid = low + (high - low)/2;
+    mergeSort(arr,low,mid);
+    mergeSort(arr,mid + 1,high);
+    merge(arr,low,mid,high);
 }
 
 main(){
-    vector<int> arr = {10,20,1,200,80,2,89,3,4,0,30};
-    print(arr);
+    vector<int> arr = {70,1,5,9,2,3,0,700};
     mergeSort(arr,0,arr.size()-1);
-    print(arr);
+    for(int num:arr)
+        cout<<num<<" ";
 }
--- revision/quick_sort_revision_one.cpp
+++ revision/quick_sort_revision_one.cpp
@@ -6,34 +6,28 @@ using namespace std;
 int partition(vector<int>&arr,int low,int high){
     int pivot = arr[high];
     int i = low - 1;
-    for(int j = low;j<=high;++j){
+    for(int j = low;j<=high;j++){
         if(arr[j]<pivot){
             ++i;
-            swap(arr[i],arr[j]);
+            swap(arr[j],arr[i]);
         }
     }
     swap(arr[i+1],arr[high]);
-    return i+1;
+    return arr[i+1];
 }
 
 void quickSort(vector<int>&arr,int low,int high){
-    if(low<high){
+    if(low>=high) return;
     int pi = partition(arr,low,high);
     quickSort(arr,low,pi-1);
     quickSort(arr,pi,high);
-    }
-}
 
-void print(vector<int>arr){
-    for(int num:arr){
-        cout<<num<<" ";
-    }
-    cout<<endl;
 }
 
 main(){
-    vector<int> arr = {10,20,1,200,80,2,89,3,4,0,30};
-    print(arr);
+    vector<int> arr = {70,1,5,9,2,3,0,700};
     quickSort(arr,0,arr.size()-1);
-    print(arr);
+    for(int num:arr){
+        cout<<num<<" ";
+    }
 }
