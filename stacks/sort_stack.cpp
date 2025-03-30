a2c6a339630fe8eeb6b3bde36ffefb08b4a29293
Ajaysvasan <ajay192006@gmail.com>
2025-03-30 16:01:40 +0530

    reversing sorting the stack

--- stacks/sort_stack.cpp
+++ stacks/sort_stack.cpp
@@ -14,6 +14,16 @@ class SortStack{
             sortedStack(s,element);
             s.push(top);
         }
+        void reverseStack(std::stack<int>&s,int element){
+            if(s.empty()||s.top()<element){
+                s.push(element);
+                return;
+            }
+            int top = s.top();
+            s.pop();
+            reverseStack(s,element);
+            s.push(top);
+        }
     public:
         void sortStack(std::stack<int>&s){
             if(s.empty()) return;
@@ -21,6 +31,14 @@ class SortStack{
             s.pop();
             sortStack(s);
             sortedStack(s,top);
+        }
+
+        void reverse_sort_stack(std::stack<int>&s){
+            if(s.empty()) return;
+            int top = s.top();
+            s.pop();
+            reverse_sort_stack(s);
+            reverseStack(s,top);
         }
 
         void print(std::stack<int>&s){
@@ -39,6 +57,6 @@ main(){
     arr.push(12);
     arr.push(1);
     arr.push(41);
-    s.sortStack(arr);
+    s.reverse_sort_stack(arr);
     s.print(arr);
 }
