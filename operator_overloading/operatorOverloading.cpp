9bbc85e0c30ef809b32b63d9b5d6b06713117c3a
Ajaysvasan <ajay192006@gmail.com>
2025-04-03 22:53:26 +0530

    getting my hands dirty with operator overloading

--- operator_overloading/operatorOverloading.cpp
+++ operator_overloading/operatorOverloading.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 #include<vector>
+#include<list>
 
-void  operator<<(std::ostream& COUT, std::vector<std::vector<int>>&arr){
+struct Details{
+    std::string name;
+    int age;
+
+    Details(std::string name,int age){
+        this->name = name;
+        this->age = age;
+    }
+
+    bool operator==(const Details& details){
+        return this->name == details.name;
+    }
+};
+
+struct Collect{
+    std::list<Details> get_details;
+    void operator+=(const Details& details){
+        this->get_details.push_back(details);
+    }
+    void operator-=(const Details& details){
+        this->get_details.remove(details);
+    }
+};
+
+std::ostream& operator<<(std::ostream& COUT, const std::vector<std::vector<int>>&arr){
     for(int i = 0;i<arr.size();i++){
         for(int j = 0;j<arr[0].size();j++){
             COUT<<arr[i][j]<<" ";
         }
         COUT<<std::endl;
         }
+        COUT<<std::endl;
+        return COUT;
     }
 
-    std::vector<std::vector<int>> operator*(std::vector<std::vector<int>>&arr1,std::vector<std::vector<int>>&arr2){
+    std::vector<std::vector<int>> operator*(const std::vector<std::vector<int>>&arr1,const std::vector<std::vector<int>>&arr2){
         if(arr1[0].size() != arr2.size()) return {{}};
         std::vector<std::vector<int>> result(arr1[0].size(), std::vector<int>(arr2.size(), 0));
         for(int i = 0;i<arr1.size();++i){
@@ -24,10 +51,33 @@ void  operator<<(std::ostream& COUT, std::vector<std::vector<int>>&arr){
 
 }
 
-int main() {
+std::ostream& operator<<(std::ostream& COUT,const Details& detail){
+    COUT<<detail.name<<std::endl;
+    COUT<<detail.age<<std::endl;
+    return COUT;
+}
+
+std::ostream& operator<<(std::ostream& COUT,const Collect& collect){
+    for(Details data: collect.get_details){
+        COUT<<data<<std::endl;
+    }
+    return COUT;
+}
+
+main() {
     std::vector<std::vector<int>> matrix_one = {{1,2,3},{4,5,6},{7,8,9}};
     std::vector<std::vector<int>> matrix_two = {{10,11,12},{13,14,15},{16,17,18}};
     std::vector<std::vector<int>> result = matrix_one*matrix_two;
-    std::cout<<result;
-    return 0;
+    std::cout<<"result Matrix\n"<<result<<"matrix One\n"<<matrix_one<<"matrix Two\n"<<matrix_two;
+
+    Details d1 = Details("Ajay",19);
+    Details d2 = Details("Anish",19);
+
+    Collect collect;
+    collect+=d1;
+    collect+=d2;
+    collect-=d2;
+
+    std::cout<<collect;
+
 }
