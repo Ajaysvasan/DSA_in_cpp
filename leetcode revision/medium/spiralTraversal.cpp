b8355020cef38b6f60ff0c6466157183b5e11133
Ajaysvasan <ajay192006@gmail.com>
2025-12-10 00:49:24 +0530

    spiral traversal

---
+++ leetcode revision/medium/spiralTraversal.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+using namespace std;
+vector<int> spiralOrder(vector<vector<int>> &matrix) {
+  vector<int> result;
+
+  int n = matrix.size();
+  int m = matrix[0].size();
+
+  int top = 0, bottom = n - 1;
+  int left = 0, right = m - 1;
+
+  while (top <= bottom && left <= right) {
+    for (int col = left; col <= right; col++)
+      result.push_back(matrix[top][col]);
+    top++;
+    for (int row = top; row <= bottom; row++)
+      result.push_back(matrix[row][right]);
+    right--;
+    if (top <= bottom) {
+      for (int col = right; col >= left; col--)
+        result.push_back(matrix[bottom][col]);
+      bottom--;
+    }
+
+    if (left <= right) {
+      for (int row = bottom; row >= top; row--)
+        result.push_back(matrix[row][left]);
+      left++;
+    }
+  }
+
+  return result;
+}
