05de6ba1c6356dbd06b51824bed96fc8469f086b
Ajaysvasan <ajay192006@gmail.com>
2026-01-05 20:57:21 +0100

    astroid collision

---
+++ leetcode revision/medium/astroidCollision.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  vector<int> asteroidCollision(vector<int> &asteroids) {
+    stack<int> st;
+
+    for (int a : asteroids) {
+      bool destroyed = false;
+
+      while (!st.empty() && st.top() > 0 && a < 0) {
+        if (abs(st.top()) < abs(a)) {
+          st.pop(); // stack asteroid explodes
+          continue;
+        } else if (abs(st.top()) == abs(a)) {
+          st.pop(); // both explode
+          destroyed = true;
+          break;
+        } else {
+          destroyed = true; // current asteroid explodes
+          break;
+        }
+      }
+
+      if (!destroyed) {
+        st.push(a);
+      }
+    }
+
+    vector<int> res;
+    while (!st.empty()) {
+      res.push_back(st.top());
+      st.pop();
+    }
+    reverse(res.begin(), res.end());
+    return res;
+  }
+};
