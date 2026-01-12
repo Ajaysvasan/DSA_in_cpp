1d1ccc35f70812c0f48c3fb05a1fd3e6689a17f0
Ajaysvasan <ajay192006@gmail.com>
2026-01-12 21:02:36 +0100

    lc 452

---
+++ leetcode revision/medium/1029.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <functional>
+#include <queue>
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int twoCitySchedCost(vector<vector<int>> &costs) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>,
+                   greater<pair<int, int>>>
+        heap;
+    int first_n = costs.size() / 2;
+    int assigned_to_a = 0;
+    int cost = 0;
+    for (int i = 0; i < costs.size(); i++) {
+      heap.push({costs[i][0] - costs[i][1], i});
+    }
+    while (!heap.empty()) {
+      int x = heap.top().second;
+      if (assigned_to_a < first_n) {
+        cost += costs[x][0];
+        assigned_to_a++;
+      } else {
+        cost += costs[x][1];
+      }
+      heap.pop();
+    }
+    return cost;
+  }
+};
